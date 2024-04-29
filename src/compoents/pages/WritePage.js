8f2ac61f274b553913896a767d3b87d9d90ea856
koreait <climax00@naver.com>
2024-04-29 20:37:06 +0900

    할 일 폼 체크

--- src/compoents/pages/WritePage.js
+++ src/compoents/pages/WritePage.js
@@ -1,13 +1,53 @@
-import { TextField } from "@mui/material";
+import { Button, TextField } from "@mui/material";
 
 export default function WritePage() {
-    return (
-      <>
-        <div className="flex-1 flex p-10 flex-col gap-7">
-          <TextField label="날짜 입력" focused type="datetime-local"/>
+  const onSubmit = (e) => {
+    e.preventDefault();
 
-          <TextField label="할 일 작성" multiline/>
-        </div>
-      </>
-    );
-  }
+    const form = e.target;
+
+    form.content.value = form.content.value.trim();
+
+    if ( form.regDate.value.length == 0 ) {
+      alert("날짜를 입력해주세요.");
+      form.regDate.focus();
+      return;
+    }
+
+    if ( form.content.value.length == 0 ) {
+      alert("날짜를 입력해주세요.");
+      form.content.focus();
+      return;
+    }
+  };
+
+  return (
+    <>
+      <form className="flex-1 flex p-10 flex-col gap-7" onSubmit={onSubmit}>
+        <TextField
+          label="날짜 입력"
+          focused
+          type="datetime-local"
+          name="regDate"
+          />
+
+        <TextField
+          label="할 일 작성" 
+          multiline
+          name="content"
+          className="flex-1 flex"
+          InputProps={{ className: "flex-1 flex-col"}}
+          inputProps={{ className: "flex-1 bg-red-500"}}
+        />
+
+        <Button type="submit" variant="contained">
+          <span>
+            <i class="fa-solid fa-pencil"></i>
+          </span>
+          <span>&nbsp;</span>
+          <span>추가하기</span>
+        </Button>
+      </form>
+    </>
+  );
+}
