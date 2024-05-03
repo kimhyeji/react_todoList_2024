14921c6bbfaca137184829a80fcdff0ebe72e4c6
koreait <climax00@naver.com>
2024-05-03 20:34:51 +0900

    완료 여부 필터링 탭메뉴 추가

--- src/compoents/pages/TodoListPage.js
+++ src/compoents/pages/TodoListPage.js
@@ -1,15 +1,56 @@
+import { Tab, Tabs } from "@mui/material";
+import { useRecoilState } from "recoil";
+
 import { useTodosState, useTodoOptionDrawerState } from  "../../hooks";
 import TodoListItem from "../TodoListItem";
 import TodoOptionDrawer from "../TodoOptionDrawer";
+import { TodoList__filterCompletedIndexAtom } from "../../atoms";
 
 export default function TodoList() {
     const todosState = useTodosState();
     const todoOptionDrawerState = useTodoOptionDrawerState();
     const onCompletedBtnClicked  = (id) => todosState.toggleTodoCompletedById(id);
+    const [filterCompletedIndex, setFilterCompletedIndex] = useRecoilState(
+        TodoList__filterCompletedIndexAtom
+      );
   
     return (
       <>
         <TodoOptionDrawer state={todoOptionDrawerState} />
+
+        <Tabs
+            variant="fullWidth"
+            value={filterCompletedIndex}
+            onChange={(event, newValue) => setFilterCompletedIndex(newValue)}
+        >
+            <Tab
+            label={
+                <span className="flex">
+                <i className="fa-solid fa-list-ul"></i>
+                <span className="ml-2">전체</span>
+                </span>
+            }
+            value={0}
+            />
+            <Tab
+            label={
+                <span className="flex">
+                <i className="fa-regular fa-square"></i>
+                <span className="ml-2">미완료</span>
+                </span>
+            }
+            value={1}
+            />
+            <Tab
+            label={
+                <span className="flex">
+                <i className="fa-regular fa-square-check"></i>
+                <span className="ml-2">완료</span>
+                </span>
+            }
+            value={2}
+            />
+        </Tabs>
   
         <div className='mt-4 px-4'>
           <ul>
